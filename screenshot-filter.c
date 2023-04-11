6fd69d1d677a9acc4f82944959f35f2ee24565d7
donna30urt <45667113+donna30urt@users.noreply.github.com>
2023-04-11 19:32:20 +0100

    Update screenshot-filter.c

--- screenshot-filter.c
+++ screenshot-filter.c
@@ -226,7 +226,7 @@ static obs_properties_t *screenshot_filter_properties(void *data)
 					   is_timer_enable_modified);
 
 	obs_properties_add_float(props, SETTING_INTERVAL, "Interval (seconds)",
-				 0.25, 86400, 0.25);
+				 0.10, 86400, 0.10);
 
 	obs_properties_add_bool(props, SETTING_RAW, "Raw image");
 
